0ec8aae9f030595db77a4ec96a023c33db717768
Ivan Sabljak <108417742+calcanthum@users.noreply.github.com>
2023-08-01 20:08:45 +0200

    Update EOP_Policy_Export.ps1

--- Exchange Online/EOP_Policy_Export.ps1
+++ Exchange Online/EOP_Policy_Export.ps1
@@ -1,3 +1,32 @@
+<#
+.SYNOPSIS
+This script connects to Exchange Online, gets the configuration of various policies, and exports the configurations to XML files.
+
+.DESCRIPTION
+The script imports the Exchange Online Management module and connects to Exchange Online without showing the banner. 
+It retrieves the name of the organization and defines a directory in the user's home folder where the policy configurations 
+will be exported to. If this directory doesn't exist, the script creates it.
+
+The script defines a hash table of policies and their corresponding "Get" commands. It then loops through each policy, retrieves 
+the policy using the respective command, and exports it to an XML file in the previously defined directory. 
+
+The file name is the name of the policy. After all the policy configurations have been exported, the script disconnects 
+from Exchange Online.
+
+.INPUTS
+No inputs. The script retrieves the necessary information internally.
+
+.OUTPUTS
+XML files representing the configuration of various policies. The files are saved in a directory under the user's home folder.
+
+.EXAMPLE
+PS C:\> .\ExportPolicyConfigs.ps1
+
+.NOTES
+The script requires that the Exchange Online Management module is installed and that the user has the necessary permissions 
+to retrieve the policy configurations.
+#>
+
 # Import the EXO V2 module
 Import-Module ExchangeOnlineManagement
 
